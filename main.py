6e2b2e3698aba6e403ffec695a1692221a6c8bba
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2021-04-13 21:36:35 -0400

    For testing...

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import game_runner
 
 if __name__ == '__main__':
     g = game_runner.Game('map.csv')
-    print(g.run(config={'debug': True}))
+    print(g.run(player_controller=controls.BlinkyController, config={'debug': True}))
 
     pygame.display.quit()
     pygame.quit()
