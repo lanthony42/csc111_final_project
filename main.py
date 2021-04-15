661223a1b369b5c9765fb0077aac773ea9ff73bf
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2021-04-15 11:28:37 -0400

    Changed main to test empty neural net.

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ TODO:
 """
 import pygame
 
+import ai_constants as const
+import ai_controls
+import ai_neural_net
 import ai_trainer
 import game_runner
 
@@ -22,11 +25,28 @@ def test_game():
     pygame.quit()
 
 
+def test_ai():
+    game = game_runner.Game('data/map.csv')
+    for _ in range(5):
+        outcome = game.run(player_controller=ai_controls.AIController,
+                           neural_net=ai_neural_net.NeuralNetGraph(const.INPUT_SIZE,
+                                                                   const.OUTPUT_SIZE),
+                           config={'is_visual': True})
+        print(outcome)
+
+        if outcome['force_quit']:
+            break
+
+    pygame.display.quit()
+    pygame.quit()
+
+
 def test_train():
     trainer = ai_trainer.AITrainer(1)
     trainer.start_training(True)
 
 
 if __name__ == '__main__':
-    test_game()
+    # test_game()
+    test_ai()
     # test_train()
