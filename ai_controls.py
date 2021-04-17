bb468591d9399427e5a050da10d9dd18b7b5145c
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2021-04-16 22:19:58 -0400

    Documented the AI representation modules.

--- ai_controls.py
+++ ai_controls.py
@@ -1,3 +1,7 @@
+"""CSC111 Final Project
+
+Module with containing the AIController class, which controls the player using a neural network.
+"""
 from copy import deepcopy
 from dataclasses import dataclass, field
 from queue import PriorityQueue
@@ -16,18 +20,44 @@ import game_controls
 
 @dataclass(order=True)
 class TileItem:
+    """A dataclass representing a tile for a PriorityQueue to sort. Note that this allows for
+    the tile to not be sorted as part of the sorting.
+
+    Instance Attributes:
+        - priority: The priority of this tile.
+        - distance: The current distance traveled to this tile.
+        - tile: The tile's position on the grid.
+    """
     priority: int
     distance: int
     tile: Vector = field(compare=False)
 
 
 class AIController(game_controls.Controller):
+    """A class representing a controller which uses a neural network to control an actor.
+
+    Instance Attributes:
+        - neural_net: The neural network associated with the controller.
+        - ticks_alive: The amount of ticks the player has survived for.
+        - last_score: A tuple containing the last score and the tick where this was achieved.
+
+    Representation Invariants:
+        - self.ticks_alive >= 0
+        - self.last_score[0] >= 0 and self.last_score[1] >= 0
+    """
     neural_net: Optional[NeuralNetGraph]
     ticks_alive: int
     last_score: tuple[int, int]
 
     def __init__(self, game: GameState, actor: Actor,
                  neural_net: Optional[NeuralNetGraph] = None) -> None:
+        """Initialize an AI controller object.
+
+        Args:
+            - game: The current game's game state object.
+            - actor: The associated actor object.
+            - neural_net: The associated neural network object.
+        """
         super().__init__(game, actor)
         self.neural_net = neural_net
 
@@ -35,8 +65,14 @@ class AIController(game_controls.Controller):
         self.last_score = (0, self.ticks_alive)
 
     def control(self, grid: list[list[int]]) -> None:
+        """Controls the player using the neural network.
+
+        Args:
+            - grid: The current game's map grid.
+        """
         directions = list(ai_const.DIRECTION_ROTATE)
 
+        # Create a list of directions around the player's current direction
         direct = self.actor.state.direction
         if direct not in directions:
             dir_index = 0
@@ -62,6 +98,13 @@ class AIController(game_controls.Controller):
             self.game.lives = 0
 
     def is_check_neural_net(self, grid: list[list[int]], directions: list[Vector]) -> bool:
+        """Returns whether or not the neural network should be checked, depending on if player
+        can turn left or right.
+
+        Args:
+            - grid: The current game's map grid.
+            - directions: The list of directions around the player's current direction.
+        """
         tile = self.actor.tile()
         left_tile = tile + directions[1]
         right_tile = tile + directions[-1]
@@ -72,11 +115,20 @@ class AIController(game_controls.Controller):
         return can_left or can_right
 
     def get_inputs(self, grid: list[list[int]], directions: list[Vector]) -> None:
+        """Updates the neural network's input nodes.
+
+        Args:
+            - grid: The current game's map grid.
+            - directions: The list of directions around the player's current direction.
+        """
         inputs = []
         tile = self.actor.tile()
+        # Targets to be searched for
         target_tiles = {g_const.WALL, g_const.DOOR, g_const.OUT, g_const.DOT, g_const.BOOST}
+        # Get ghost positions
         targets = [ghost.actor.tile() for ghost in self.game.ghosts() if ghost.state != 'inactive']
 
+        # Get inputs for every direction around the player
         for direction in directions:
             next_tile = tile + direction
 
@@ -119,38 +171,66 @@ class AIController(game_controls.Controller):
 
     def a_star_distance(self, grid: list[list[int]], targets: list[Vector],
                         direction: Vector) -> int:
+        """By treating the grid as a representation of a graph, A Star is used to return shortest
+        distance needed to travel to get to a target position in direction.
+
+        Args:
+            - grid: The current game's map grid which is used like a graph.
+            - targets: The target tiles to look for.
+            - direction: The direction to check distance for.
+        """
+        # Sets up priority queue and copy of grid to track visited nodes.
         path_grid = deepcopy(grid)
         tile_queue = PriorityQueue()
 
         tile = self.actor.tile()
+        # Don't allow revisiting of initial tile.
         path_grid[tile.y][tile.x] = g_const.OUT
 
+        # Only append to queue if next direction isn't a bad tile
         next_tile = tile + direction
         if path_grid[next_tile.y][next_tile.x] not in g_const.BAD_TILES:
             tile_queue.put(TileItem(0, 0, next_tile))
 
+        # Loop through until queue empty or target found.
         while not tile_queue.empty():
             item = tile_queue.get()
             distance = item.distance + 1
 
+            # Check to see if each direction can be added.
             for candidate in g_const.DIRECTION.values():
                 next_tile = item.tile + candidate
 
                 if next_tile in targets:
+                    # Target found!
                     return distance
                 elif path_grid[next_tile.y][next_tile.x] not in g_const.BAD_TILES:
+                    # Use heuristic function as part of the given tile's priority.
                     heuristic = self.distance_heuristic(next_tile, targets)
                     tile_queue.put(TileItem(heuristic + distance, distance, next_tile))
 
                     path_grid[next_tile.y][next_tile.x] = g_const.OUT
 
+        # If no path found.
         return -1
 
     @staticmethod
     def distance_heuristic(position: Vector, targets: list[Vector]) -> int:
+        """Underestimates the real distance by returning the smallest grid distance to a target.
+
+        Args:
+            - position: Starting position for distance.
+            - targets: A list of target vectors as endpoints for distance.
+        """
         return min(grid_distance(position, target) for target in targets)
 
     def control_outputs(self, grid: list[list[int]], directions: list[Vector]) -> None:
+        """Taking the neural network's output nodes, move in an according direction.
+
+        Args:
+            - grid: The current game's map grid.
+            - directions: The list of directions around the player's current direction.
+        """
         net_int = [node.value for node in self.neural_net.output_nodes][:ai_const.OUTPUT_SIZE]
         max_value = max(net_int)
 
@@ -159,10 +239,15 @@ class AIController(game_controls.Controller):
             self.actor.change_direction(grid, directions[dir_index + 1])
 
     def reset(self) -> None:
+        """Resets the last score value for when associated actor dies."""
         self.last_score = (self.game.score, self.ticks_alive)
 
     def draw_debug(self, screen: pygame.Surface) -> None:
-        pass
+        """Draws out debugging information. Not needed for this controller!
+
+        Args:
+            - screen: The pygame game screen.
+        """
 
 
 if __name__ == '__main__':
@@ -175,7 +260,7 @@ if __name__ == '__main__':
     })
 
     import python_ta.contracts
-    python_ta.contracts.DEBUG_CONTRACTS = True
+    python_ta.contracts.DEBUG_CONTRACTS = False
     python_ta.contracts.check_all_contracts()
 
     import doctest
--- ai_neural_net.py
+++ ai_neural_net.py
@@ -1,3 +1,9 @@
+"""CSC111 Final Project
+
+Module with containing the NeuralNetGraph class, which acts as the brain of the AI controller.
+This module is based off the graph code from the Course Nodes and Assignment 3, though
+heavily modified.
+"""
 from __future__ import annotations
 
 from typing import Union
@@ -10,7 +16,17 @@ import ai_constants as const
 
 
 class _WeightedVertex:
-    """A vertex in a graph.
+    """A weighted vertex in the neural network.
+
+    Instance Attributes:
+        - number: The number of the vertex.
+        - kind: The type of node within the neural network.
+        - value: The node's outputted value.
+        - neighbors: The node's neighbours as a dict mapping the neighbor vertex to edge weight
+
+    Representation Invariants:
+        - self.number >= 0
+        - self.kind in {'input', 'hidden', 'output'}
     """
     number: int
     kind: str
@@ -22,8 +38,12 @@ class _WeightedVertex:
         """Initialize a new vertex with the given number.
 
         Preconditions:
-            - number > 0
+            - number >= 0
             - kind in {'input', 'hidden', 'output'}
+
+        Args:
+            - number: The number of the vertex.
+            - kind: The type of node within the neural network.
         """
         self.number = number
         self.kind = kind
@@ -32,40 +52,73 @@ class _WeightedVertex:
         self.neighbours = {}
 
     def get_connections(self) -> list[tuple[int, int, float]]:
+        """Recursively returns a list of all connects of the node and its neighbors.
+        Returns a list of tuples containing the initial node, the end node, and the weight of edge.
+
+        This implementation is valid because the neural network is a directed acyclic graph,
+        thus the base case is naturally the input nodes.
+        """
+        # Input nodes are the base case.
         if self.kind == 'input':
             return []
 
         out = []
         for node, weight in self.neighbours.items():
+            # Represent connections as the initial node, the end node, and weight of edge.
             out.append((self.number, node.number, weight))
+            # Recursively get connections.
             out.extend(node.get_connections())
 
         return out
 
     def get_neighbor_numbers(self) -> set[int]:
+        """Returns the numbers of all the node's neighbors."""
         return set(neighbor.item for neighbor in self.neighbours)
 
 
 class NeuralNetGraph:
-    """A graph representing a neural network.
-    """
-    _vertices: dict[int, _WeightedVertex]
+    """A directed acyclic graph class representing a neural network.
 
+    Instance Attributes:
+        - input_nodes: A list of the input nodes of the neural network.
+        - output_nodes: A list of the output nodes of the neural network.
+        - fitness: The fitness of the neural network implementation.
+
+    Representation Invariants:
+        - self.fitness >= 0
+    """
     input_nodes: list[_WeightedVertex]
     output_nodes: list[_WeightedVertex]
-    fitness: int
+    fitness: float
+
+    # Private Instance Attributes:
+    #  - _vertices : A mapping of the node's number to the vertex itself
+    _vertices: dict[int, _WeightedVertex]
 
     def __init__(self, input_size: int, output_size: int, hidden_size: int = 1) -> None:
-        """Initialize a graph with given amount of each node types."""
+        """Initialize a graph with given amount of each node types.
+
+        Preconditions:
+            - input_size >= 0
+            - output_size >= 0
+            - hidden_size >= 0
+
+        Args:
+            - input_size: The amount of input nodes.
+            - output_size: The amount of output nodes.
+            - hidden_size: The amount of hidden nodes.
+        """
         self._vertices = {}
 
         self.input_nodes = []
         self.output_nodes = []
         self.fitness = 0
 
+        # Add input nodes.
         for _ in range(input_size):
             self.add_input_node()
 
+        # Add and connect the hidden nodes.
         hidden_nodes = []
         for i in range(hidden_size):
             hidden_nodes.append(self._vertices[self.add_hidden_node()])
@@ -73,6 +126,7 @@ class NeuralNetGraph:
             for input_node in self.input_nodes:
                 hidden_nodes[i].neighbours[input_node] = random.uniform(-1, 1)
 
+        # Add and connect the output nodes.
         for _ in range(output_size):
             vertex = self._vertices[self.add_output_node()]
 
@@ -80,7 +134,7 @@ class NeuralNetGraph:
                 vertex.neighbours[hidden_node] = random.uniform(-1, 1)
 
     def add_input_node(self) -> int:
-        """Add an input node with the given number to this graph.
+        """Add an input node to this graph and return the number.
 
         The new input node is not adjacent to any other vertices.
         """
@@ -93,7 +147,7 @@ class NeuralNetGraph:
         return num
 
     def add_hidden_node(self) -> int:
-        """Add a hidden node with the given item to this graph.
+        """Add a hidden node to this graph and return the number.
 
         The new hidden node is not adjacent to any other vertices.
         """
@@ -104,7 +158,7 @@ class NeuralNetGraph:
         return num
 
     def add_output_node(self) -> int:
-        """Add an output node with the given item to this graph.
+        """Add an output node to this graph and return the number.
 
         The new output node is not adjacent to any other vertices.
         """
@@ -122,6 +176,16 @@ class NeuralNetGraph:
         creating a directed graph.
 
         Raise a ValueError if number1 or number2 do not appear as vertices in this graph.
+        Thus, this aspect won't be included in preconditions.
+
+        Preconditions:
+            - number1 >= 0
+            - number2 >= 0
+
+        Args:
+            - number1: The number for the starting vertex.
+            - number2: The number for the ending vertex.
+            - weight: The weight of the new edge.
         """
         if number1 in self._vertices and number2 in self._vertices:
             v1 = self._vertices[number1]
@@ -137,6 +201,14 @@ class NeuralNetGraph:
         """Return the weight of the edge between the given numbers, from number1 to number2.
 
         Return 0 if number1 and number2 are not adjacent.
+
+        Preconditions:
+            - number1 in self._vertices.keys()
+            - number2 in self._vertices.keys()
+
+        Args:
+            - number1: The number for the starting vertex.
+            - number2: The number for the ending vertex.
         """
         v1 = self._vertices[number1]
         v2 = self._vertices[number2]
@@ -144,6 +216,9 @@ class NeuralNetGraph:
         return v1.neighbours.get(v2, 0)
 
     def get_connections(self) -> list[tuple[int, int, float]]:
+        """Return all the connections within the neural net.
+        Returns a list of tuples containing the initial node, the end node, and the weight of edge.
+        """
         out = []
         for node in self.output_nodes:
             out.extend(node.get_connections())
@@ -151,32 +226,60 @@ class NeuralNetGraph:
         return out
 
     def get_hidden_count(self) -> int:
+        """Return the amount of hidden nodes in the neural net."""
         return len(self._vertices) - len(self.input_nodes) - len(self.output_nodes)
 
     def propagate_outputs(self) -> None:
+        """The main function to update all values for the output nodes."""
         for node in self.output_nodes:
             self._propagate_node(node, set())
 
     def _propagate_node(self, curr_node: _WeightedVertex, visited: set()) -> None:
+        """Updates the values of all nodes curr_node relies on, then updates its value.
+
+        Is a recursive helper function for propagate_outputs, relying on the natural base case
+        of the input nodes, given the neural network is a directed acyclic graph.
+
+        Preconditions:
+            - curr_node.number not in visited
+
+        Args:
+            - curr_node: The current node to be updated.
+            - visited: The set of node numbers corresponding to already visited thus updated nodes.
+        """
+        # Input nodes as base case
         if curr_node.kind == 'input':
             return
 
         visited.add(curr_node.number)
         value = 0
 
+        # Update all nodes curr_node relies on (The node's neighbors).
         for node, weight in curr_node.neighbours.items():
             if node.number not in visited:
                 self._propagate_node(node, visited)
             value += node.value * weight
 
+        # Sigmoid activation function!
         curr_node.value = scipy.special.expit(value)
 
     def get_mutated_child(self, best_fitness: float) -> NeuralNetGraph:
+        """Returns a copy of the graph with slightly mutated edge weights.
+
+        Preconditions:
+            - best_fitness >= 0
+
+        Args:
+            - best_fitness: The best fitness for the training.
+        """
+        # Start making a copy of this neural network
         new_network = NeuralNetGraph(len(self.input_nodes), len(self.output_nodes),
                                      self.get_hidden_count())
-        factor = 1 / (const.WEIGHT_CO - max(0, const.WEIGHT_OFFSET - \
-                                            best_fitness / const.FITNESS_CO))
+        # Modification factor depends on best fitness - better fitness, more precise mutations.
+        factor = 1 / (const.WEIGHT_CO - max(const.WEIGHT_OFFSET - best_fitness / const.FITNESS_CO,
+                                            0))
 
+        # Add all connections with mutations
         for v1, v2, weight in self.get_connections():
             if random.uniform(0, 1) < const.RANDOM_CHANCE:
                 weight = random.uniform(-1, 1)
@@ -188,12 +291,23 @@ class NeuralNetGraph:
 
 
 def load_neural_network(file_path: str) -> NeuralNetGraph:
+    """Returns neural network from the csv file at file_path.
+
+    Preconditions:
+        - file_path is a valid path to a csv file.
+
+    Args:
+        - file_path: The path for a csv file storing the neural network representation.
+    """
     with open(file_path) as csv_file:
         reader = csv.reader(csv_file)
         initial_sizes = next(reader)
 
+        # First row of csv are the sizes of the node types.
         neural_net = NeuralNetGraph(int(initial_sizes[0]), int(initial_sizes[1]),
                                     int(initial_sizes[2]))
+
+        # Add all the rows as edge connections.
         for connection in reader:
             neural_net.add_edge(int(connection[0]), int(connection[1]), float(connection[2]))
 
@@ -201,11 +315,22 @@ def load_neural_network(file_path: str) -> NeuralNetGraph:
 
 
 def save_neural_network(neural_net: NeuralNetGraph, file_path: str) -> None:
+    """Saves the neural network as a csv file.
+
+    Preconditions:
+        - file_path is a valid path for a csv file, which may or may not exist yet.
+
+    Args:
+        - neural_net: The neural network to be saved.
+        - file_path: The path for a csv file storing the neural network representation.
+    """
     with open(file_path, 'w+', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
+        # Output the sizes of the node types.
         connections = [(len(neural_net.input_nodes), len(neural_net.output_nodes),
                         neural_net.get_hidden_count())]
 
+        # Write the connections.
         writer.writerows(connections + neural_net.get_connections())
 
 
